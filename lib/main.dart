09f87073796b1d0d4d822db2c7dbc533a692cc74
Haris <muhamad_haris@hotmail.com>
2020-11-03 12:59:26 +0800

    Add answer

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,68 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
+                IconButton(icon: Icon(Icons.extension), onPressed: () {}),
+              ],
+            ),
+            SizedBox(height: 37),
+            Text.rich(
+              TextSpan(
+                  text: 'Welcome,',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  children: [
+                    TextSpan(
+                      text: ' Charlie',
+                      style: TextStyle(fontWeight: FontWeight.normal),
+                    ),
+                  ]),
+              style: TextStyle(fontSize: 50),
+            ),
+            SizedBox(height: 30),
+            TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
+                prefixIcon: Icon(Icons.search, size: 18),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: 'Search',
+              ),
+            ),
+            SizedBox(height: 80),
+            Text(
+              'Saved Places',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+            ),
+            Container(
+              height: 300,
+              child: GridView.count(
+                padding: EdgeInsets.only(top: 20),
+                childAspectRatio: 1.491,
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                children: [
+                  for (var country in countries)
+                    Image.asset('assets/images/$country.png')
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
